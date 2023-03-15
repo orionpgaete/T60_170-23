95200ee4779667f4d0b38da6bd0c3bfc79efd771
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-03-15 20:39:30 -0300

    clase 1

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Ingrese nombre:");
+            //esto es un Scanner
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese edad: ");
+            string edad = Console.ReadLine();
+            Console.WriteLine("Su nombre es " + nombre + "la edad es" + edad);
+            Console.WriteLine("visto de otra manera {0} y la edad es {1}", nombre, edad);
+            Console.ReadKey();
+        }
+    }
+}
