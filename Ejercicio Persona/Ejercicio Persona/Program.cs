4481758d4cc0fb597e8c75faf36eac80e1692d44
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-03-21 15:34:43 -0300

    ejercicio persona

---
+++ Ejercicio Persona/Ejercicio Persona/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Persona
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            uint telefono;
+            double peso;
+            double estatura;
+            bool esValido;
+
+            Console.WriteLine(" Bienvenido al programa del IMC");
+
+
+            do
+            {
+                Console.WriteLine("Ingrese nombre");
+                nombre = Console.ReadLine().Trim();
+            } while (nombre.Equals(string.Empty));
+
+            do
+            {
+                Console.WriteLine("Ingrese telefono");
+                esValido = UInt32.TryParse(Console.ReadLine().Trim(), out telefono);
+            } while (!esValido);
+
+            do
+            {
+                Console.WriteLine("Ingrese estatura");
+                esValido = Double.TryParse(Console.ReadLine().Trim(), out estatura);
+            } while (!esValido);
+
+            do
+            {
+                Console.WriteLine("Ingrese Peso");
+                esValido = Double.TryParse(Console.ReadLine().Trim(), out peso);
+            } while (!esValido);
+
+            Console.WriteLine("Nombre: {0}", nombre);
+            Console.WriteLine("Telefono: {0}", telefono);
+            Console.WriteLine("Estatura: {0}", estatura);
+            Console.WriteLine("Peso: {0}", peso);
+            Console.WriteLine("IMC: {0}", peso/(estatura*estatura));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- Ejercicio_1/Ejercicio_1/Program.cs
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -15,10 +15,52 @@ namespace Ejercicio_1
             //esto es un Scanner
             string nombre = Console.ReadLine();
             Console.WriteLine("Ingrese edad: ");
-            string edad = Console.ReadLine();
-          //  Console.WriteLine("Su nombre es " + nombre + "la edad es " + edad);
-            Console.WriteLine("Su nombre es {0} y la edad es {1}", nombre, edad);
- 
+            string edadTx = Console.ReadLine().Trim();
+           
+            int edad = -1;
+
+            bool esValido = Int32.TryParse(edadTx, out edad);
+
+            if (!esValido)
+            {
+                Console.WriteLine("Ingrese bien la edad");
+            }else
+            {
+                Console.WriteLine("Su nombre es {0} y la edad es {1}", nombre, edad);
+            }
+
+
+
+
+
+
+            // TRIM = '   32   ' => '32';
+            // trimstart = '   32   ' => '32   ';
+            //trimend = '   32   ' => '   32';
+
+            //  Console.WriteLine("Su nombre es " + nombre + "la edad es " + edad);
+
+
+            // Forma 1 tradiciona
+
+            /* string num = "12";
+             int num2 = int.Parse(num);   //12
+
+             try
+             {
+                 string num = "12";
+                 int num2 = Convert.ToInt32(num);   //12
+             }
+             catch (Exception e) { }
+
+             string num = "12";
+             int num2;
+
+             bool esValido = int.TryParse(num, out num2);
+            */
+
+
+
             Console.ReadKey();
         }
     }
