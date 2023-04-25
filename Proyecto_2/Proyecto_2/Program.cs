48738ee876089932ff90b0e1d76cdb20b9132b8b
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-04-25 14:03:02 -0400

    server socket

--- Proyecto_2/Proyecto_2/Program.cs
+++ Proyecto_2/Proyecto_2/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Proyecto_2.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,33 @@ namespace Proyecto_2
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+            if (servidor.Iniciar())
+            {
+                //OK, puede conectar
+                Console.WriteLine("servidor iniciado");
+                while (true)
+                {
+                    Console.WriteLine("Esperando Cliente");
+                    Socket socketCliente = servidor.ObtenerCliente();
+                    //Construir mecanismo de escritura y lectura
+                    ClienteCom cliente = new ClienteCom(socketCliente);
+                    //ahora el protocolo de cominicacion 
+                    cliente.Escribir("Hola Mundo Cliente, dame tu nombre");
+                    string respuesta = cliente.Leer();
+                    Console.WriteLine("El cliente mando: {0}", respuesta);
+                    cliente.Escribir(" Chaooo, loh juimos");
+                    cliente.Desconectar();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error, el puerto {0} esta en uso", puerto);
+
+            }
         }
     }
 }
